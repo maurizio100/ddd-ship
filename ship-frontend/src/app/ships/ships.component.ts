2de10abfdea5c988970bfb9f4d7da15e5726df07
Maurizio Rinder <maurizio.rinder@willhaben.at>
2022-09-16 21:07:22 +0200

    add dockerfile to build application

--- ship-frontend/src/app/ships/ships.component.ts
+++ ship-frontend/src/app/ships/ships.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShipsComponent implements OnInit {
 
-  ship = "Black Pearl";
+  ship = "Flying Dutchman";
   
   constructor() { }
 
