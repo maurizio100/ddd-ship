c824a258131f8ba87cef8a251f00a4bcb78b4b25
Maurizio Rinder <maurizio.rinder@willhaben.at>
2022-09-17 13:52:16 +0200

    create endpoint for creating a ship

--- ship-backend/driving-adapter/src/main/kotlin/at/willhaben/driving/adapter/web/ShipController.kt
+++ ship-backend/driving-adapter/src/main/kotlin/at/willhaben/driving/adapter/web/ShipController.kt
@@ -1,22 +1,25 @@
 package at.willhaben.driving.adapter.web
 
-import org.springframework.beans.factory.annotation.Value
+import at.willhaben.domain.ports.driving.ShipCreationDataDTO
+import at.willhaben.domain.ports.driving.ShipManagementPort
+import at.willhaben.driving.adapter.web.requestmodel.ShipCreationRequest
+import at.willhaben.driving.adapter.web.responsemodel.ShipResponse
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/web")
 @CrossOrigin(origins = ["http://localhost:4200"])
 class ShipController (
-    @Value("\${api.hello.value}") val hellValue: String
+        private val shipManagementPort: ShipManagementPort
 ){
 
     @PostMapping("/ships")
-    fun createShip(@RequestBody ship: Ship): Ship {
-        return ship.copy(id = 40)
+    fun createShip(@RequestBody ship: ShipCreationRequest): ShipResponse {
+        val shipCreationDTO = ShipCreationDataDTO(name = ship.name);
+        val createdShip = shipManagementPort.createShip(shipCreationDTO)
+        return ShipResponse(
+            id = createdShip.id,
+            name = createdShip.name
+        )
     }
-
-    data class Ship(
-        val id: Long?,
-        val name: String
-    )
 }
