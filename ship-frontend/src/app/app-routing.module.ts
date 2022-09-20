7e551838130c32103f06c38c7d58c7ee58809a61
Maurizio Rinder <maurizio.rinder@willhaben.at>
2022-09-20 14:11:40 +0200

    add ship detail page and routing

---
+++ ship-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ShipsComponent } from './ships/ships.component';
+import { ShipDetailComponent } from './ship-detail/ship-detail.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/ships', pathMatch: 'full' },
+  { path: 'ships', component: ShipsComponent },
+  { path: 'detail/:id', component: ShipDetailComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
