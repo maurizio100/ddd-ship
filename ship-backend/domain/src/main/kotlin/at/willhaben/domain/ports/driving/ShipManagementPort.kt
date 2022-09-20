f4f29f9160408c0b866305e15341e96a4053be78
Maurizio <moonrider1510@gmail.com>
2022-09-20 15:49:59 +0200

    move ship querying functionality to ShipInformationService.kt (#57)
    Co-authored-by: Maurizio Rinder <maurizio.rinder@willhaben.at>

--- ship-backend/domain/src/main/kotlin/at/willhaben/domain/ports/driving/ShipManagementPort.kt
+++ ship-backend/domain/src/main/kotlin/at/willhaben/domain/ports/driving/ShipManagementPort.kt
@@ -2,6 +2,5 @@ package at.willhaben.domain.ports.driving
 
 interface ShipManagementPort {
     fun createShip(shipCreationData: ShipCreationDataDTO): ShipDTO
-    fun getAllShips(): List<ShipDTO>
     fun deleteShip(shipId: Long)
 }
--- ship-backend/domain/src/main/kotlin/at/willhaben/domain/service/ShipInformationService.kt
+++ ship-backend/domain/src/main/kotlin/at/willhaben/domain/service/ShipInformationService.kt
@@ -1,7 +1,9 @@
 package at.willhaben.domain.service
 
+import at.willhaben.domain.converter.ShipConverter
 import at.willhaben.domain.model.Ship
 import at.willhaben.domain.ports.driven.ShipQueryPort
+import at.willhaben.domain.ports.driving.ShipDTO
 import at.willhaben.domain.ports.driving.ShipDetailDTO
 import at.willhaben.domain.ports.driving.ShipInformationPort
 import org.springframework.stereotype.Service
@@ -11,15 +13,14 @@ import java.lang.IllegalStateException
 class ShipInformationService(
     private val shipQueryPort: ShipQueryPort
 ): ShipInformationPort {
-    override fun getShipDetails(shipId: Long): ShipDetailDTO? {
-        return shipQueryPort.getShipDetails(shipId)?.let {
-            toShipDetailDTO(it)
-        }
+
+    override fun getAllShips(): List<ShipDTO> {
+        return shipQueryPort.getAllShips().map { ShipConverter.toShipDTO(it)}
     }
 
-    private fun toShipDetailDTO(ship: Ship) =
-        ShipDetailDTO(
-            id = ship.id ?: throw IllegalStateException(),
-            name = ship.shipName
-        )
+    override fun getShipDetails(shipId: Long): ShipDetailDTO? {
+        return shipQueryPort.getShipDetails(shipId)?.let {
+            ShipConverter.toShipDetailDTO(it)
+        }
+    }
 }
