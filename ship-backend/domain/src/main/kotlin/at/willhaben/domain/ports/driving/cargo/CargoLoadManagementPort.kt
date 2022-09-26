5edbc9189d3a7d8c288643c5b3b9e3585e3a0b90
Maurizio <moonrider1510@gmail.com>
2022-09-26 14:13:55 +0200

    add port definition for unloading ship (#98)
    Co-authored-by: Maurizio Rinder <maurizio.rinder@willhaben.at>

--- ship-backend/domain/src/main/kotlin/at/willhaben/domain/ports/driving/cargo/CargoLoadManagementPort.kt
+++ ship-backend/domain/src/main/kotlin/at/willhaben/domain/ports/driving/cargo/CargoLoadManagementPort.kt
@@ -1,8 +1,8 @@
 package at.willhaben.domain.ports.driving.cargo
 
-import at.willhaben.domain.ports.driving.ship.ShipDTO
 import at.willhaben.domain.ports.driving.ship.ShipDetailDTO
 
 interface CargoLoadManagementPort {
     fun addCargo(shipId: Long, cargoId: Long): ShipDetailDTO?
+    fun removeCargo(shipId: Long, cargoId: Long): ShipDetailDTO?
 }
