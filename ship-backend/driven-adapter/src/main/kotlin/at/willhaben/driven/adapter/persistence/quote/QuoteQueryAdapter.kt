feea9bc5e0517b208a332fea80f92e131a7eca21
Maurizio <moonrider1510@gmail.com>
2022-09-28 18:43:58 +0200

    persist shipping and query quote (#123)
    Co-authored-by: Maurizio Rinder <maurizio.rinder@willhaben.at>

--- ship-backend/driven-adapter/src/main/kotlin/at/willhaben/driven/adapter/persistence/quote/QuoteQueryAdapter.kt
+++ ship-backend/driven-adapter/src/main/kotlin/at/willhaben/driven/adapter/persistence/quote/QuoteQueryAdapter.kt
@@ -1,11 +1,16 @@
 package at.willhaben.driven.adapter.persistence.quote
 
 import at.willhaben.domain.ports.driven.QuoteQueryPort
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 
 @Component
-class QuoteQueryAdapter(): QuoteQueryPort {
+class QuoteQueryAdapter(
+    private val quoteRepository: QuoteRepository
+): QuoteQueryPort {
     override fun getQuoteForSailorsCode(sailorsCode: Int): String {
-        TODO("Not yet implemented")
+        return quoteRepository.findByIdOrNull(sailorsCode.toLong())?.let {
+            it.quote
+        } ?: "Yeah go and sail!"
     }
 }
