540cd5a3a68a47004ce314b620f39273b43b350e
Maurizio Rinder <maurizio.rinder@willhaben.at>
2022-09-29 09:44:24 +0200

    make services work from external access

--- ship-frontend/src/app/cargo.service.ts
+++ ship-frontend/src/app/cargo.service.ts
@@ -9,7 +9,7 @@ import { tap } from 'rxjs';
   providedIn: 'root'
 })
 export class CargoService {
-  private cargosUrl = 'http://localhost:8080/web/cargos';
+  private cargosUrl = 'http://172.22.118.168:8080/web/cargos';
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
