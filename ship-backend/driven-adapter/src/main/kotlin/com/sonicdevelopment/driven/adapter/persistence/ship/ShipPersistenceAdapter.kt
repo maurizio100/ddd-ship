6b86078a61fecd6efb5c713981b6d7835591a734
Maurizio <moonrider1510@gmail.com>
2024-05-29 20:44:32 +0200

    149 delete shippings when ship gets deleted

--- ship-backend/driven-adapter/src/main/kotlin/com/sonicdevelopment/driven/adapter/persistence/ship/ShipPersistenceAdapter.kt
+++ ship-backend/driven-adapter/src/main/kotlin/com/sonicdevelopment/driven/adapter/persistence/ship/ShipPersistenceAdapter.kt
@@ -3,11 +3,14 @@ package com.sonicdevelopment.driven.adapter.persistence.ship
 import com.sonicdevelopment.domain.model.Ship
 import com.sonicdevelopment.domain.ports.driven.ShipPersistencePort
 import com.sonicdevelopment.driven.adapter.persistence.cargo.CargoRepository
+import com.sonicdevelopment.driven.adapter.persistence.shipping.ShippingRepository
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Component
 
 @Component
 class ShipPersistenceAdapter(
     private val shipRepository: ShipRepository,
+    private val shippingRepository: ShippingRepository,
     private val cargoRepository: CargoRepository
 ): ShipPersistencePort {
     override fun save(ship: Ship): Ship {
@@ -27,7 +30,9 @@ class ShipPersistenceAdapter(
         }
     }
 
+    @Transactional
     override fun delete(shipId: Long) {
+        shippingRepository.deleteByShip_Id(shipId)
         shipRepository.deleteById(shipId)
     }
 }
--- ship-backend/driven-adapter/src/main/kotlin/com/sonicdevelopment/driven/adapter/persistence/shipping/ShippingRepository.kt
+++ ship-backend/driven-adapter/src/main/kotlin/com/sonicdevelopment/driven/adapter/persistence/shipping/ShippingRepository.kt
@@ -3,4 +3,5 @@ package com.sonicdevelopment.driven.adapter.persistence.shipping
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ShippingRepository: JpaRepository<ShippingPersistenceEntity, Long> {
+    fun deleteByShip_Id(shipId: Long)
 }
