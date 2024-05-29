2d826dedd470c301af28b1311971ebf346e840f7
Maurizio <moonrider1510@gmail.com>
2024-05-29 21:54:56 +0200

    150 make persistence of shipping more readable

--- ship-backend/driven-adapter/src/main/kotlin/com/sonicdevelopment/driven/adapter/persistence/shipping/ShippingPersistenceAdapter.kt
+++ ship-backend/driven-adapter/src/main/kotlin/com/sonicdevelopment/driven/adapter/persistence/shipping/ShippingPersistenceAdapter.kt
@@ -1,8 +1,11 @@
 package com.sonicdevelopment.driven.adapter.persistence.shipping
 
 import com.sonicdevelopment.domain.model.Ship
+import com.sonicdevelopment.domain.model.Shipping
 import com.sonicdevelopment.domain.ports.driven.ShippingPersistencePort
+import com.sonicdevelopment.driven.adapter.persistence.ship.ShipPersistenceEntity
 import com.sonicdevelopment.driven.adapter.persistence.ship.ShipRepository
+import jakarta.transaction.Transactional
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 
@@ -11,19 +14,35 @@ class ShippingPersistenceAdapter(
     private val shippingRepository: ShippingRepository,
     private val shipRepository: ShipRepository
 ): ShippingPersistencePort {
-    override fun createShipping(ship: Ship): Ship {
-        val persistedShip = shipRepository.findByIdOrNull(ship.id) ?: return ship
 
-        val shipping = ShippingPersistenceEntity(
-            id = ship.shipping?.id,
-            sailorsCode = ship.shipping?.sailorsQuote!!,
+    @Transactional
+    override fun createShipping(ship: Ship): Ship {
+        val shippingRecord = ship.shipping ?: throw IllegalStateException()
+
+        val persistedShippingRecord: ShippingPersistenceEntity = shippingRecord.id?.let { shippingRepository.findByIdOrNull(it) }?.let {
+            updateShippingRecord(shippingRecord, it)
+        } ?: createShippingRecord(ship, shippingRecord)
+
+        shippingRecord.id = persistedShippingRecord.id
+        return ship
+    }
+
+    private fun createShippingRecord(ship: Ship, shippingRecord: Shipping): ShippingPersistenceEntity {
+        val persistedShip = shipRepository.findByIdOrNull(ship.id) ?: throw IllegalStateException()
+        val shippingPersistenceEntity = ShippingPersistenceEntity(
+            sailorsCode = shippingRecord.sailorsQuote,
             ship = persistedShip
         )
-        val persistedShipping = shippingRepository.save(shipping)
-        persistedShip.shipping = persistedShipping
 
+        val persistedShipping = shippingRepository.save(shippingPersistenceEntity)
+        persistedShip.shipping = persistedShipping
         shipRepository.save(persistedShip)
-        ship.shipping?.id = persistedShipping.id
-        return ship
+
+        return persistedShipping
+    }
+
+    private fun updateShippingRecord(shippingRecord: Shipping, shippingPersistenceEntity: ShippingPersistenceEntity): ShippingPersistenceEntity {
+        shippingPersistenceEntity.sailorsCode = shippingRecord.sailorsQuote
+        return shippingRepository.save(shippingPersistenceEntity)
     }
 }
