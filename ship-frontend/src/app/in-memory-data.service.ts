f5a70c9528c0abcdc64d33b0229a28cc1bafcbaa
Maurizio <moonrider1510@gmail.com>
2025-02-14 16:23:08 +0100

    style ship items

--- ship-frontend/src/app/in-memory-data.service.ts
+++ ship-frontend/src/app/in-memory-data.service.ts
@@ -5,53 +5,117 @@ import { Cargo } from './cargo';
 import { ShippingSummary } from './shipping-summary';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InMemoryDataService implements InMemoryDbService {
-
   createDb() {
     const ships: Ship[] = [
-      { id: 12, name: 'Dr. Nice', weight: 3.0, maxweight: 20, cargo: [
-        {id: 1, name:'Ale', weight: 1.0},
-        { id: 2, name: 'Chocolate', weight: 1.0},
-        { id: 3, name: 'Cinnamon', weight: 1.0}
-      ]},
+      {
+        id: 12,
+        name: 'Dr. Nice',
+        weight: 3.0,
+        maxweight: 20,
+        cargo: [
+          { id: 1, name: 'Ale', weight: 1.0 },
+          { id: 2, name: 'Chocolate', weight: 1.0 },
+          { id: 3, name: 'Cinnamon', weight: 1.0 },
+        ],
+      },
 
-      { id: 14, name: 'Dr. Nice', weight: 3.0, maxweight: 20, cargo: [
-        { id: 7, name: 'Paprika', weight: 1.0},
-        { id: 8, name: 'Planks', weight: 1.0},
-        { id: 9, name:'Rum', weight: 1.0},
-        { id: 10, name: 'Silk', weight: 1.0}
-      ] },
-     ];
-    
-    const cargos: Cargo[] = [
-      { id: 1, name:'Ale', weight: 1.0},
-      { id: 2, name: 'Chocolate', weight: 1.0},
-      { id: 3, name: 'Cinnamon', weight: 1.0},
-      { id: 4, name: 'Coffee', weight: 1.0},
-      { id: 5, name:'Fruits', weight: 1.0},
-      { id: 6, name: 'Leather', weight: 1.0},
-      { id: 7, name: 'Paprika', weight: 1.0},
-      { id: 8, name: 'Planks', weight: 1.0},
-      { id: 9, name:'Rum', weight: 1.0},
-      { id: 10, name: 'Silk', weight: 1.0},
-      { id: 11, name: 'Sugar', weight: 1.0},
-      { id: 12, name: 'Tobacco', weight: 1.0},
-      { id: 13, name:'Wheat', weight: 1.0},
-      { id: 14, name: 'Wine', weight: 1.0}
+      {
+        id: 14,
+        name: 'Dr. Nice',
+        weight: 3.0,
+        maxweight: 20,
+        cargo: [
+          { id: 7, name: 'Paprika', weight: 1.0 },
+          { id: 8, name: 'Planks', weight: 1.0 },
+          { id: 9, name: 'Rum', weight: 1.0 },
+          { id: 10, name: 'Silk', weight: 1.0 },
+        ],
+      },
+      {
+        id: 12,
+        name: 'Dr. Nice',
+        weight: 3.0,
+        maxweight: 20,
+        cargo: [
+          { id: 1, name: 'Ale', weight: 1.0 },
+          { id: 2, name: 'Chocolate', weight: 1.0 },
+          { id: 3, name: 'Cinnamon', weight: 1.0 },
+        ],
+      },
+
+      {
+        id: 14,
+        name: 'Dr. Nice',
+        weight: 3.0,
+        maxweight: 20,
+        cargo: [
+          { id: 7, name: 'Paprika', weight: 1.0 },
+          { id: 8, name: 'Planks', weight: 1.0 },
+          { id: 9, name: 'Rum', weight: 1.0 },
+          { id: 10, name: 'Silk', weight: 1.0 },
+        ],
+      },
+      {
+        id: 12,
+        name: 'Dr. Nice',
+        weight: 3.0,
+        maxweight: 20,
+        cargo: [
+          { id: 1, name: 'Ale', weight: 1.0 },
+          { id: 2, name: 'Chocolate', weight: 1.0 },
+          { id: 3, name: 'Cinnamon', weight: 1.0 },
+        ],
+      },
+
+      {
+        id: 14,
+        name: 'Dr. Nice',
+        weight: 3.0,
+        maxweight: 20,
+        cargo: [
+          { id: 7, name: 'Paprika', weight: 1.0 },
+          { id: 8, name: 'Planks', weight: 1.0 },
+          { id: 9, name: 'Rum', weight: 1.0 },
+          { id: 10, name: 'Silk', weight: 1.0 },
+        ],
+      },
     ];
 
-    const shippings: ShippingSummary[] =[
-      { id: 12, name: 'Dr. Nice', cargo: [
-        {id: 1, name:'Ale', weight: 1.0},
-        { id: 2, name: 'Chocolate', weight: 1.0},
-        { id: 3, name: 'Cinnamon', weight: 1.0}
-      ], sailorsCode: 'Sailing is fun for everyone!'
-    }];
+    const cargos: Cargo[] = [
+      { id: 1, name: 'Ale', weight: 1.0 },
+      { id: 2, name: 'Chocolate', weight: 1.0 },
+      { id: 3, name: 'Cinnamon', weight: 1.0 },
+      { id: 4, name: 'Coffee', weight: 1.0 },
+      { id: 5, name: 'Fruits', weight: 1.0 },
+      { id: 6, name: 'Leather', weight: 1.0 },
+      { id: 7, name: 'Paprika', weight: 1.0 },
+      { id: 8, name: 'Planks', weight: 1.0 },
+      { id: 9, name: 'Rum', weight: 1.0 },
+      { id: 10, name: 'Silk', weight: 1.0 },
+      { id: 11, name: 'Sugar', weight: 1.0 },
+      { id: 12, name: 'Tobacco', weight: 1.0 },
+      { id: 13, name: 'Wheat', weight: 1.0 },
+      { id: 14, name: 'Wine', weight: 1.0 },
+    ];
 
-    return {ships, cargos, shippings};
+    const shippings: ShippingSummary[] = [
+      {
+        id: 12,
+        name: 'Dr. Nice',
+        cargo: [
+          { id: 1, name: 'Ale', weight: 1.0 },
+          { id: 2, name: 'Chocolate', weight: 1.0 },
+          { id: 3, name: 'Cinnamon', weight: 1.0 },
+        ],
+        sailorsCode: 'Sailing is fun for everyone!',
+      },
+    ];
+
+    return { ships, cargos, shippings };
   }
-  
-  constructor() { }
+
+  constructor() {}
 }
